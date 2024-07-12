211882fb9ec083c05aa18a98fa7ea48e1e0e98bc
Jones J Alapat <jonesjalapat@gmail.com>
2024-07-12 10:10:46 -0400

    initial springboot

---
+++ src/main/java/com/hireapro/admin/service/HireaproApplication.java
@@ -0,0 +1,13 @@
+package com.hireapro.admin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hireapro/admin/service/HireaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireapro.admin.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
